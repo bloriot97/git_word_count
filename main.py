b2f4e2eb9f1a0bbbc84e395e956ee38f458435be
bloriot97 <bloriot97@gmail.com>
2018-12-23 17:08:52 +0100

    Add lines and regression

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 import datetime
 
 def read_file(path):
@@ -13,6 +14,8 @@ def read_file(path):
 
 dir = './Rapport-TN09'
 file = 'main.tex'
+due_date = datetime.datetime.strptime('2019-02-11 00:00:00+01:00', '%Y-%m-%d %H:%M:%S%z')
+
 
 if __name__ == '__main__':
     repo = Repo('./Rapport-TN09')
@@ -31,6 +34,29 @@ if __name__ == '__main__':
         length = len(read_file(file_path))
         lengths.append(length)
 
+    def get_fit_fn(dates, lengths, dim):
+        min_date = min(dates)
+        deltas = [ (date - min_date).total_seconds() for date in dates]
+        fit = np.polyfit(deltas, lengths, dim)
+        print(fit)
+        def fit_fn(date):
+            x = (date - min_date).total_seconds()
+            return  sum([ n * np.power(x, p) for p, n in enumerate(reversed(fit))])
+        return fit_fn
     fig, ax = plt.subplots()
-    ax.plot(dates, lengths)
+
+    fit_fn = get_fit_fn(dates, lengths, 1)
+    base = min(dates)
+    date_list = [base + datetime.timedelta(days=x) for x in range(0, 100)]
+    prediction = [fit_fn(date) for date in date_list]
+
+    ax.plot(
+        dates, lengths, '-b',
+        date_list, prediction, '--r'
+    )
+
+    ax.axhline(y=100000, linestyle='--')
+    ax.axhline(y=120000, linestyle='--')
+    ax.axvline(x=due_date, color='orange')
+
     plt.savefig('plot.png')
