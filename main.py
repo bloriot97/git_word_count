ae9037261a167244f6da4a80aeb98734434f87c4
bloriot97 <bloriot97@gmail.com>
2018-12-29 21:26:28 +0100

    n last commit for the regression

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def read_file(path):
         return file.read()
     return ''
 
-dir = './Rapport-TN09'
+dir = '../Rapport-TN09'
 file = 'main.tex'
 due_date = datetime.datetime.strptime('2019-02-11 00:00:00+01:00', '%Y-%m-%d %H:%M:%S%z')
 
 
 if __name__ == '__main__':
-    repo = Repo('./Rapport-TN09')
+    repo = Repo(dir)
     repo.git.checkout('master')
     commits = list(repo.iter_commits())
     file_path = os.path.join(dir, file)
@@ -34,6 +34,8 @@ if __name__ == '__main__':
         length = len(read_file(file_path))
         lengths.append(length)
 
+    repo.git.checkout('master')
+
     def get_fit_fn(dates, lengths, dim):
         min_date = min(dates)
         deltas = [ (date - min_date).total_seconds() for date in dates]
@@ -45,9 +47,13 @@ if __name__ == '__main__':
         return fit_fn
     fig, ax = plt.subplots()
 
-    fit_fn = get_fit_fn(dates, lengths, 1)
-    base = min(dates)
-    date_list = [base + datetime.timedelta(days=x) for x in range(0, 100)]
+    n_last_commit = 10
+    dates_for_regression = dates[:n_last_commit]
+    lengths_for_regression = lengths[:n_last_commit]
+    fit_fn = get_fit_fn(dates_for_regression, lengths_for_regression, 1)
+    base = min(dates_for_regression)
+    delta_days =  int((due_date - min(dates_for_regression)).total_seconds() / (3600*24))
+    date_list = [base + datetime.timedelta(days=x) for x in range(0, delta_days + 3)]
     prediction = [fit_fn(date) for date in date_list]
 
     ax.plot(
